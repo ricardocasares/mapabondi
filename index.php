3e4dee4af4c24577b9bc9c8c9458ba45e5efcd1e
Ricardo Casares <ricardocasares@gmail.com>
2013-12-18 17:10:25 -0300

    structure

--- index.php
+++ index.php
@@ -2,10 +2,12 @@
 
 require 'vendor/autoload.php';
 
-$app = new \Slim\Slim();
+$app = new \Slim\Slim(array(
+	'templates.path' => 'views'
+));
 // main route
-$app->get('/', function () {
-	echo "Welcome to mapabondi";
+$app->get('/', function () use ($app) {
+	$app->render('layout.php',array());
 });
 
 $app->run();

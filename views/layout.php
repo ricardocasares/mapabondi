d65c631a58346a596bd9bb79a46c0dfdfb1fd6b3
Ricardo Casares <ricardocasares@gmail.com>
2013-12-20 20:47:07 -0300

    viewport

--- views/layout.php
+++ views/layout.php
@@ -2,7 +2,9 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'>
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="description" content="">
     <meta name="author" content="">
 

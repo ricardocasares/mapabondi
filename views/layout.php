527b85411837c36bc171ab264b2b6b053492e5f1
Ricardo Casares <ricardocasares@gmail.com>
2013-12-22 17:27:57 -0300

    footer links

--- views/layout.php
+++ views/layout.php
@@ -81,7 +81,7 @@
             <span class="mapabondi">
               <i class="fa fa-map-marker"></i> mapabondi
             </span>
-            &copy; <?php echo date('Y') ?> <a href="http://betamonster.com.ar/">Betamonster</a> · <a href="https://github.com/ricardocasares/mapabondi">Source</a>
+            &copy; <?php echo date('Y') ?> <a href="http://betamonster.com.ar/">Betamonster</a> · <a href="https://github.com/ricardocasares/mapabondi">Source</a> · <a href="https://github.com/ricardocasares/mapabondi/wiki/API-Documentation">API</a>
           </p>
         </div>
         <!-- /footer -->
